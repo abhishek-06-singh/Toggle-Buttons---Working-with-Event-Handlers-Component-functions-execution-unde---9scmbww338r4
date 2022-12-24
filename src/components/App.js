1a805621519ab9729eef488e3cfa06e91def1bab
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2022-12-24 12:09:22 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-//code here 
-  
+  //code here
+  const [text, setText] = useState(true);
+  const handleClick = () => {
+    setText(!text);
+  };
 
-  
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+        {text ? "OFF" : "ON"}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
+        {!text ? "OFF" : "ON"}
       </button>
     </div>
   );
-}
-
+};
 
 export default App;
